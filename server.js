fbef0d0183f8ba9a941bdd1feddeabe7af299b9f
Joaquin Huerta <joaco.011@gmail.com>
2022-11-15 07:52:31 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        age: 29,
+        birthName: 'Something',
+        birthLocation: 'London England'
+    },
+    'chance the rapper':{
+        age: 30,
+        birthName: 'Chancelors',
+        birthLocation: 'Illinois'
+    },
+    'dylan':{
+        age: 'Dylan',
+        birthName: 'Dylan',
+        birthLocation: 'Dylan'
+    },
+}
+
+
+app.get('/',(request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const rappersName = request.params.rapperName.toLowerCase();
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
